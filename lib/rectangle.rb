0677683bbf620764c76c0a89ca63dbc81e17ceaa
Ian Knox <knoxid@gmail.com>
2014-07-30 09:17:22 -0600

    rectangle returns its l and w

---
+++ lib/rectangle.rb
@@ -0,0 +1,10 @@
+class Rectangle
+
+  def initialize(l, w)
+    @l = l
+    @w = w
+  end
+
+  attr_accessor :l, :w
+
+end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,2 +1,10 @@
 require 'spec_helper'
 
+describe "Rectangle" do
+  it "return its length and width" do
+    rec = Rectangle.new(5, 6)
+
+    expect(rec.l).to eq(5)
+    expect(rec.w).to eq(6)
+  end
+end
