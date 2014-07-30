7c9e0b69d61ea4d80a69e8142f2d692b4c07b829
Ian Knox <knoxid@gmail.com>
2014-07-30 09:28:36 -0600

    square can be initialized and return its side

---
+++ lib/square.rb
@@ -0,0 +1,8 @@
+class Square
+
+  def initialize(side)
+    @side = side
+  end
+
+  attr_accessor :side
+end
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -1,2 +1,10 @@
 require 'spec_helper'
 
+describe "Square" do
+  sq = Square.new(5)
+
+  it "returns its side" do
+    expect(sq.side).to eq(5)
+  end
+end
+
